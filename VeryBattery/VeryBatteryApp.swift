2e5da0139cf91c9114deda104a5846660b4df807
john <nam9295@gmail.com>
2026-03-26 09:14:21 +0900

    Initial Commit

---
+++ VeryBattery/VeryBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeryBatteryApp.swift
+//  VeryBattery
+//
+//  Created by 남형우 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct VeryBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
